425af73b5694d5ce748a1d9ee1f721230a47080e
Midhet Sulemani <midhetfatema94@gmail.com>
2017-05-03 18:10:57 +0530

    fix: removed unnecessary files

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+import { Actions } from 'react-native-router-flux';
 
 const RouterComponent = () => {
     return (
         <Router sceneStyle = {{ paddingTop: 65 }}>
-            <Scene key="login" component={LoginForm} title="Please Login" />
-            <Scene key="employeeList" component={EmployeeList} title="Employee List" initial/>
+            <Scene key="auth">
+                <Scene key="login" component={LoginForm} title="Please Login" />
+            </Scene>
+
+            <Scene key="main">
+                <Scene 
+                    onRight = {() => Actions.employeeCreate()}
+                    rightTitle = "Add"
+                    key="employeeList" 
+                    component={EmployeeList} 
+                    title="Employee List"
+                    initial
+                />
+                <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee"/>
+            </Scene>
         </Router>
     );
 };
