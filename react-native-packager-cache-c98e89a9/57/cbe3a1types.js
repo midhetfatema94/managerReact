2619b299f884a10abd0c5c47c574b65ae76c53d1
Midhet Sulemani <midhetfatema94@gmail.com>
2017-05-03 18:15:01 +0530

    update: added gitignore file

--- react-native-packager-cache-c98e89a9/57/cbe3a1types.js
+++
@@ -1,8 +0,0 @@
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var EMAIL_CHANGED = exports.EMAIL_CHANGED = 'email_changed';
-var PASSWORD_CHANGED = exports.PASSWORD_CHANGED = 'password_changed';
-var LOGIN_USER_SUCCESS = exports.LOGIN_USER_SUCCESS = 'login_user_success';
-var LOGIN_USER_FAIL = exports.LOGIN_USER_FAIL = 'login_user_fail';
-var LOGIN_USER = exports.LOGIN_USER = 'login_user';
